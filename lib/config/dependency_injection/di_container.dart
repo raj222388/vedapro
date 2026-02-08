d09729a5ffdf1f21dd1d22c100ff02eedd48fd37
RAJ <gondsushilkumar6@gmail.com>
2026-02-08 18:45:57 +0530

    Create di_container.dart with GetIt setup and service registration

---
+++ lib/config/dependency_injection/di_container.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+// Initialize the service locator.
+final GetIt getIt = GetIt.instance;
+
+void setup() {
+  // Register services here.
+  // For example:
+  // getIt.registerLazySingleton<YourService>(() => YourServiceImpl());
+
+  // Add your service registrations here
+}
